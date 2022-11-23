dab19eaf0d8bde6fb4d5bc60321f2d1739b7566d
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2022-11-23 10:42:51 +0100

    better project instantiation

--- lib/main.dart
+++ lib/main.dart
@@ -12,16 +12,21 @@ import 'package:app/home_page.dart';
 void main() async {
   // prefs is a global variable in logic.dart
   prefs = await SharedPreferences.getInstance();
-  final projects = prefs!.getString("projects")?.split(";") ?? [];
-  for (final project in projects) {
-    if (project.isEmpty) continue;
-    final attr = project.split(",");
-    final dir = Directory(attr.last);
-    dir.exists().then((dirExists) {
-      // XXX: Das ist besser als projc.addProject,
-      // da wir sicher sein können, dass Projects schon in preferences sind
-      projC.projects.add(Project.add(attr.first, dir));
-    });
+  final projectsString = prefs!.getString("projects")?.trim();
+  if (projectsString?.isNotEmpty ?? false){
+    final projects = projectsString!.split(";");
+    await Future.wait(
+      [
+        for (final project in projects) 
+          () async {
+            final attr = project.split(",");
+            final dir = Directory(attr.last);
+            if (await dir.exists()) {
+              projC.projects.add(Project.add(attr.first, dir));
+            }
+          } ()
+      ]
+    );
   }
 
   final darkMode = prefs!.getBool("darkMode") ?? false;
