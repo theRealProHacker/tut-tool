fcd594418eb1446a65c076f00acaa64e9dac4c96
theRealProHacker <77074862+theRealProHacker@users.noreply.github.com>
2022-12-02 17:11:20 +0100

    sync comments with files and grade indicator

--- lib/io.dart
+++ lib/io.dart
@@ -195,6 +195,20 @@ void runFile(File file) {
   }
 }
 
+// https://gist.github.com/thosakwe/681056e86673e73c4710cfbdfd2523a8
+Future<void> copyDirectory(Directory source, Directory destination) async {
+  await for (var entity in source.list(recursive: false)) {
+    if (entity is Directory) {
+      var newDirectory =
+          Directory(p.join(destination.absolute.path, p.basename(entity.path)));
+      await newDirectory.create();
+      await copyDirectory(entity.absolute, newDirectory);
+    } else if (entity is File) {
+      await entity.copy(p.join(destination.path, p.basename(entity.path)));
+    }
+  }
+}
+
 /// Converts file2Text
 Future<String> file2Text(File file) async {
   // TODO: unpack zip files
